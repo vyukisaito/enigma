c0988b860a154d06f6e7d5b492c44db6b7ba6535
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-07 10:49:56 -0300

    enigma

--- fase5/js/script.js
+++ fase5/js/script.js
@@ -4,8 +4,8 @@ let resposta = document.getElementById('resposta')
 function res() {
     let answer = prompt('Digite sua resposta:')
 
-    if (answer === 'menino jesus' || 'menino deus') {
-        window.location.href = ''
+    if (answer === 'menino jesus' || 'menino deus' || 'menino de Deus' || 'menino Jesus') {
+        window.location.href = 'https://vyukisaito.github.io/enigma/fase6/'
     } else if (answer === '' || answer === null) {
         alert('pq não preencheu???');
     } else {
