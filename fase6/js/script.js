8f4d77ec6f3c8c8b73f4d0a142be9be345269d84
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-06 22:26:15 -0300

    fase6

--- fase6/js/script.js
+++ fase6/js/script.js
@@ -4,7 +4,7 @@ let resposta = document.getElementById('resposta')
 function res() {
     let answer = prompt('Digite sua resposta:')
 
-    if (answer === 'menino jesus' || 'menino deus') {
+    if (answer === 'not') {
         window.location.href = ''
     } else if (answer === '' || answer === null) {
         alert('pq não preencheu???');
