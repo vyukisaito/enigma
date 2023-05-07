41b184439d91e6dc031b0da8f99b09925f04c767
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-07 10:48:53 -0300

    enigma

--- fase2/js/script.js
+++ fase2/js/script.js
@@ -4,8 +4,8 @@ let resposta = document.getElementById('resposta')
 function res() {
     let answer = prompt('Digite sua resposta:')
 
-    if (answer === 'cidade de deus') {
-        window.location.href = ''
+    if (answer === 'cidade de deus' || 'cidade de Deus') {
+        window.location.href = 'https://vyukisaito.github.io/enigma/fase3/'
     } else if (answer === '' || answer === null) {
         alert('pq não preencheu???');
     } else {
