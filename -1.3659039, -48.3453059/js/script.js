6f87072ced9d1d4fedd80820e697f4603f8ca2a5
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-05-12 21:21:13 -0300

    update

--- -1.3659039, -48.3453059/js/script.js
+++ -1.3659039, -48.3453059/js/script.js
@@ -8,7 +8,7 @@ function res() {
         return; // encerra a função se a resposta for 'null'
     }
 
-    if (answer === 'menino jesus' || answer === 'menino deus' || answer === 'menino jesus ' || answer === 'menino deus ') {
+    if (answer === 'menino jesus' || answer === 'menino deus' || answer === 'menino jesus ' || answer === 'menino deus ' || answer === 'menino de jesus' || answer === 'menino de deus' || answer === 'menino de jesus ' || answer === 'menino de deus') {
         window.location.href = 'https://vyukisaito.github.io/enigma/direção/'
     } else if (answer === '') {
         alert('pq não preencheu???');
